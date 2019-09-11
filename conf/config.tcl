5fb549f2ea37eaa2c3dd02145ef7e76c77ecf6fa
Biblepix <biblepix@vollmar.ch>
2019-09-11 17:20:15 +0000

    firstPush

---
+++ conf/config.tcl
@@ -0,0 +1,15 @@
+#set auftragDir $env(HOME)/Kontor
+#cd $auftragDir
+
+set HOME $env(HOME)
+set db kontordb
+set dbname kontordb
+set dbuser postgres
+set company "Vollmar Übersetzungs-Service"
+set spoolDir [file join $auftragDir spool]
+set dumpDir [file join $auftragDir dumps]
+set vorlage [file join $spoolDir rechnung-vorlage.rtf]
+set printCmd bas
+
+
+
